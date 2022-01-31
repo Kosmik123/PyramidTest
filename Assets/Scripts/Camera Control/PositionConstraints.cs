c590dcd62a1e28de403c42438c1d92d20ead0715
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-01-31 07:58:10 +0100

    Minor project directory changes

--- Assets/Scripts/Camera Control/PositionConstraints.cs
+++ Assets/Scripts/Camera Control/PositionConstraints.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PyramidGamesTest
+namespace PyramidGamesTest.CameraControl
 {
     public class PositionConstraints : MonoBehaviour
     {
--- Assets/Scripts/Camera Control/RTSCameraController.cs
+++ Assets/Scripts/Camera Control/RTSCameraController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace PyramidGamesTest
+namespace PyramidGamesTest.CameraControl
 {
     public class RTSCameraController : MonoBehaviour
     { 
