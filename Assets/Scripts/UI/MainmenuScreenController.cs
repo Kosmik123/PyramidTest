984ad9610fd46b9b5f14b0baf85f017267f7bde5
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-02-01 00:08:54 +0100

    Adde Main Menu screen controller

---
+++ Assets/Scripts/UI/MainmenuScreenController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+namespace PyramidGamesTest.UI
+{
+    public class MainmenuScreenController : MonoBehaviour
+    {
+        public TextMeshProUGUI bestTimeIndicator;
+
+        private void OnEnable()
+        {
+            bestTimeIndicator.text = UIHelper.FormatTime(GameManager.instance.bestPlaytime);
+        }
+    }
+}
