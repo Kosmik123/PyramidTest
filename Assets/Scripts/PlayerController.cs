292113ea917d18f5f6ab8539c993cd6f0a4a1b7e
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-01-31 19:00:44 +0100

    Created PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PyramidGamesTest
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public CameraControl.RTSCameraController cameraController;
+
+        private void OnEnable()
+        {
+            GameManager.OnGameStarted += ResetPlayer;
+            GameManager.OnGameFinished += DisableCameraController;
+        }
+        private void ResetPlayer()
+        {
+            cameraController.enabled = true;
+            cameraController.transform.position = Vector3.zero;
+        }
+
+        private void DisableCameraController()
+        {
+            cameraController.enabled = false;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.OnGameStarted -= ResetPlayer;
+            GameManager.OnGameFinished -= DisableCameraController;
+        }
+    }
+}
