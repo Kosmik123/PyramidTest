de90ac25c9ef8577d91db319657c6e63873475ec
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-01-31 17:40:48 +0100

    Added utility scripts
    Added FileManager scrip
    Added Singleton script

---
+++ Assets/Scripts/Utility/FileManager.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+
+public static class FileManager
+{
+    public static void WriteSaveFile(string contents, string filepath)
+    {
+        File.WriteAllText(filepath, contents);
+    }
+
+    public static void WriteSaveFile<T>(T data, string filepath)
+    {
+        FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate);
+        BinaryFormatter formatter = new BinaryFormatter();
+        try
+        {
+            Debug.Log("Serializing " + filepath);
+            formatter.Serialize(fs, data);
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogError("Save failded! " + e);
+            throw;
+        }
+        finally
+        {
+            fs.Close();
+        }
+    }
+
+    public static string ReadSaveFileAsString(string filepath)
+    {
+        if (File.Exists(filepath))
+        {
+            return File.ReadAllText(filepath);
+        }
+        return "";
+    }
+
+    public static T ReadSaveFile<T>(string filepath)
+    {
+        FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate);
+        BinaryFormatter formatter = new BinaryFormatter();
+        T data = default;
+
+        try
+        {
+            data = (T)formatter.Deserialize(fs);
+        }
+        catch (SerializationException e)
+        {
+            Debug.LogWarning("Loading error! " + e);
+        }
+        finally
+        {
+            fs.Close();
+        }
+
+        return data;
+    }
+
+    public static bool Exists(string filepath)
+    {
+        return Directory.Exists(filepath);
+    }
+
+}
---
+++ Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Singleton
+{
+    public static T MakeInstance<T>(T _this, T instance) where T : Object
+    {
+        if (instance != null && instance != _this)
+        {
+            Debug.LogWarning(_this.name + " has been deleted");
+            Object.Destroy(_this);
+            if(_this is MonoBehaviour)  
+                Object.Destroy((_this as MonoBehaviour).gameObject);
+        }
+        else
+        {
+            instance = _this;
+        }
+        return instance;
+    }
+}
