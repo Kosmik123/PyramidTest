5ba9e268f257f8c041dc65019a6b9be685d42818
Kosmik123 <malpa.w.klapkach@gmail.com>
2022-02-02 02:55:25 +0100

    Added missing Chest script

---
+++ Assets/Scripts/Interactions/Chest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace PyramidGamesTest.Interactions
+{
+    public class Chest : InteractableObject
+    {
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void Open()
+        {
+            GiveChoice("Open?", () => animator.SetBool("Open", true));
+        }
+
+
+    }
+}
